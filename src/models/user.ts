b54c31c4f9bebe82ed435e27a342f21f50ca4f50
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2024-07-17 23:27:55 -0700

    Create the app's models directory and include the user model

---
+++ src/models/user.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+// Define the user schema with required fields.
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['student', 'instructor'],
+        default: 'student',
+    },
+});
+
+// Create a mongoose model based on the user schema.
+const User = mongoose.model('User', userSchema);
+
+export default User;
