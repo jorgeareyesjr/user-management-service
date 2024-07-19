9e80fa7ff58fcf2716dcbfef3f448e0828bfe656
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2024-07-19 15:33:58 -0700

    Update the active supported server routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import {
     createUser,
     // deleteUser,
-    getUser,
+    // getUser,
     logInUser,
     // logOutUser,
     // updateUser,
@@ -13,9 +13,10 @@ const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', logInUser);
-router.get('/:username', getUser);
-// router.put('/:username', authenticateUser, updateUser);
-// router.delete('/:username', authenticateUser, deleteUser);
+// @TODO: Add routes for the following:
+// router.get('/profile', authenticateUser, getUser);
+// router.put('/profile/update/:username', authenticateUser, updateUser);
+// router.delete('/profile/delete/:username', authenticateUser, deleteUser);
 // router.get('/logout', authenticateUser, logOutUser);
 
 export default router;
