6d3ff180de05440dd5d1e450daf89f32c4aaa796
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2024-07-19 15:32:35 -0700

    Update the email within the User model to no longer be required to be unique

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // Define the user schema with required fields.
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: false },
     password: { type: String, required: true },
     role: {
         type: String,
